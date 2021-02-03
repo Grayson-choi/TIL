be3f9cee0cb6ee6b8eeb7a16b47fe1ece482a527
Grayson-choi <784wldnd@gmail.com>
2021-02-03 13:52:37 +0900

    vue.js study

---
+++ vue/test/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Header />
+    <div id="content" class="content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "../components/layout/Header.vue";
+
+export default {
+  name: 'App',
+  component: {
+    Header,
+ }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue/test/src/views/Home.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <h1>Welcome {{title2}}!</h1>
+    <input type="text" v-model="input1">
+    <button type="button" @click="getData">GET</button>
+    <button type="button" @click="setData">SET</button>
+
+    <select class="form-control" v-model="region" @change="changeRegion">
+      <option :key="i" :value="d.v" v-for="(d, i) in options">{{d.t}}</option>
+    </select>
+
+    <table class="table table-bordered" v-if="tableShow">
+      <tr :key="i" :value="d.v" v-for="(d, i) in options">
+        <td>{{d.v}}</td>
+        <td>{{d.t}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Vue.js 입문 강의",
+      title2: "Seoul",
+      input1: "abcd",
+      options: [
+        {v:"S", t:"Seoul"},
+        {v:"J", t:"Jeju"},
+        {v:"B", t:"Busan"},
+        ],
+        region: "B",
+        tableShow: true,
+    };
+    },
+    watch: {
+      input1() {
+         console.log(this.input1);
+        },
+    },
+
+    methods: {
+      getData() {
+          alert(this.input1);
+        },
+      setData() {
+        this.input1 = "12345";
+      },
+      changeRegion() {
+        alert(this.region);
+      }
+    },
+    beforeCreate() {
+      console.log("beforeCreated");
+    },
+    created() {
+      console.log("Created");
+    },
+    beforeMount() {
+      console.log("beforeMount");
+    },
+    mounted() {
+      console.log("mounted");
+    }, 
+    beforeUpdate(){
+      console.log("beforeUpdate");
+    },
+    updated(){
+      console.log("updated");
+    },
+    beforeDestroy() {
+      console.log("beforeDestroy");
+    },
+    destroyed(){
+      console.log("destroyed");
+    }
+}
+</script>
